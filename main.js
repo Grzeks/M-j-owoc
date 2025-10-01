3ce23af955b9ae443a706ec238755a2246a1e700
Grzeks <darekgrzegorczykwidzew@gmail.com>
2025-10-01 13:57:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyEnBhqiYyn8RE85VtN-vir2BRv7XrLkO3SL60eUi9fGU_e_HNwW49hNMjUTbBwSqldXQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzI7yD-quq_rAfTSSbCHj5qE5TsjfssOkq50XEhzyKGLgf2BZopD8vJ8D236MvzQtoydw/exec";
 
 const monthSelect = document.getElementById("month-select");
 const monthTitle = document.getElementById("month-title");
@@ -159,6 +159,7 @@ monthSelect.addEventListener("change", () => {
 
 // start
 loadMonths();
+
 
 
 
